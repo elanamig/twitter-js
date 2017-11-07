731aa58aa11dc00a220fb50a5298240ca5f09f3a
elanamig <{elanamig}@users.noreply.github.com>
2017-11-07 12:23:44 -0500

    getting there

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express applicatio
 const nunjucks = require('nunjucks');
+const routes = require('./routes');
 
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
@@ -11,20 +12,6 @@ app.listen(3000, () => {
     console.log("Up and running on 3000");
 });
 
-
-//nunjucks.render()
-
-app.use('/', (req, res, next )=>{
-    console.log(req.method + ' '+req.url + ' '+res.statusCode);
-    next ();
-});
-
-app.use ('/special', (req, res, next) => {
-    console.log("Reached special");
-    next();
-})
-
-app.get('/', (req, res) => {
-    res.render('index.html', {title: 'this is a title', people: [{name: 'Vasya'}, {name: 'Petya'}, {name: 'Masha'}]});
-});
+app.use(express.static('public'));
+app.use('/', routes);
 
